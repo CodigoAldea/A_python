0454e57fc1eaf1f58298a7c5a1611d9acf8b3b94
CodigoAldea <dmdndrd@gmail.com>
2022-09-13 13:47:04 +0530

    new

---
+++ list/ques1.py
@@ -0,0 +1,8 @@
+# Write a Python program to sum all the items in a list.
+
+l = [ 10, 30, 50 ]
+#b = len(l)
+sum = 0
+for i in l:
+    sum = sum + i
+print("Sum of all the elements : ",sum)
---
+++ list/ques2.py
@@ -0,0 +1,7 @@
+#  Write a Python program to multiply all the items in a list.
+
+l = [ 10, 30, 50 ]
+m = 1
+for i in l:
+    m = m * i
+print("Product of all the elements : ",m)
