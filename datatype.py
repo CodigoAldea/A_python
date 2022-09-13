b814e2c833807f3dcb4d05fb4ed14a62cb68fff8
CodigoAldea <dmdndrd@gmail.com>
2022-09-13 13:46:55 +0530

    new

--- datatype.py
+++ datatype.py
@@ -25,8 +25,47 @@
             c. Slicing 
         4. nested
         5. Mutable 
+    
+    Tuples:
+        Syntax : var_name = (v1,v2 ,v3 ....)
+        1. ordered 
+        2. immutable
+        3. Index
+            a. positive
+            b. negetive 
+            c. Slicing 
+        4. Assignment, Packing and Unpacking
+        
+    String: 
+        syntax: var_name = "string"   we can use both " or ' .
+        str() : to convert in string
+        len() : to check the number of characters or elements
+        ord() : convers a char into an int.
+        f-string : Formatted string 
+            syntax : f'string chars {l} string chars'
+        Immutable.
+        replace() : to replace a char with another
+        join() : conver a list to string. 
+        split() : converts string to list.
+        byte object : 
+            a = b'string'
+    
+    Dictionaries : 
+        Syntax : var = {key:value , key:value, key:value ....}
+            key can never be a list or another dictionary
+        
+        mutable 
+        dynamic
+        nested
 '''
+d = {
+    'name':'Python',
+    "dob":'00000',
+    "email":"abc@abc.com"     
+    }
+d1 = { "dob":'88888',
+    "email":"xyz@abc.com",
+    "acb": " 333"}
 
-# how to read a list 
-l = ["a","b","c","d","e",'f','g']
-print(l[1::2])
+d.update(d1)
+print(d)
