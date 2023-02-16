7878e18b813c1e49ec79e66048525ecb611cde60
CodigoAldea <dmdndrd@gmail.com>
2023-02-16 16:53:13 +0530

    new

---
+++ Class_Feb_2023/if.py
@@ -0,0 +1,22 @@
+'''
+    1. if 
+    2. if - else
+    3. if - elif - else
+    
+    nested if 
+    
+    Syntax : 
+        if condition :
+            if statement
+        else:
+            else statement
+'''
+a = 6
+b = 10 
+c = 30
+if a > b and a>c :
+    print(a)
+elif b>c:
+    print(b)
+else:
+    print(c)
