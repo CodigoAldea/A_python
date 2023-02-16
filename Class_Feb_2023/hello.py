77215e45255d032a19a34c16e49fd8895a771e42
CodigoAldea <dmdndrd@gmail.com>
2023-02-16 16:52:28 +0530

    new

---
+++ Class_Feb_2023/hello.py
@@ -0,0 +1,31 @@
+# datatype in python
+'''
+    1 interger
+    2 float
+    3 complex 
+    4 boolean 
+    5 set 
+    6 sequence 
+        1 string 
+        2 list 
+        3 tuple 
+    7 dictionary  Mapping data type 
+'''
+a = "hello"
+b = 'world'
+'''
+syntax for slicing 
+var_name[start : stop : step]
+'''
+print(len(a))
+print(a[:2:2])
+
+'''
+    function is a code block. 
+    2 type : 
+        1. pre defined 
+        2. user defined 
+        
+    Syntax for pre-defined function : 
+        func_name()   this is known as function call.
+'''
